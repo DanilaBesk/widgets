9aa419b7766c5b3924c0739e4f4867cd7ac06b22
kiriyyan <kiriyan54@gmail.com>
2025-08-05 07:18:26 +0300

    refactor: added api get request and added actions to get user geolocation

--- src/models/widgets/WeatherWidjet.tsx
+++ src/models/widgets/WeatherWidjet.tsx
@@ -1,43 +1,80 @@
-import { useState, useEffect } from "react"
-import axios from "axios"
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-
+interface WeatherCodeMessage {
+  [code: number]: string;
+}
 
 const WeatherWidget = () => {
-    const [weather, setWeather] = useState('')
-    const [temperature, setTemperature] = useState(0)
+  const [weather, setWeather] = useState(0);
+  const [temperature, setTemperature] = useState("0");
+  const [latitude, setLatitude] = useState(55.75);
+  const [longitude, setLongtitude] = useState(37.62);
 
-    
+  const weatherCodeMessage: WeatherCodeMessage = {
+    0: "Ясно ☀️",
+    1: "В основном ясно 🌤️",
+    2: "Переменная облачность 🌥️",
+    3: "Пасмурно ☁️",
+    45: "Туман 🌫️",
+    48: "Туман (изморозь) 🌫️",
+    51: "Лёгкая морось 🌦️",
+    53: "Умеренная морось 🌦️",
+    55: "Сильная морось 🌦️",
+    56: "Лёгкая ледяная морось 🌧️❄️",
+    57: "Сильная ледяная морось 🌧️❄️",
+    61: "Лёгкий дождь 🌧️",
+    63: "Умеренный дождь 🌧️",
+    65: "Сильный дождь 🌧️",
+    66: "Лёгкий ледяной дождь 🌧️❄️",
+    67: "Сильный ледяной дождь 🌧️❄️",
+    71: "Лёгкий снег 🌨️",
+    73: "Умеренный снег 🌨️",
+    75: "Сильный снег 🌨️",
+    77: "Снежные зёрна ❄️",
+    80: "Лёгкий ливень 🌦️",
+    81: "Умеренный ливень 🌦️",
+    82: "Сильный ливень ⛈️",
+    85: "Лёгкий снегопад 🌨️",
+    86: "Сильный снегопад 🌨️",
+    95: "Гроза 🌩️",
+    96: "Гроза с лёгким градом ⛈️",
+    99: "Гроза с сильным градом ⛈️❄️",
+  };
 
-    useEffect(() => {
-        async function getWeather() {
-        try{
+  const currentWeatherMessage: string =
+    weatherCodeMessage[weather] || "Неизветная погода";
+
+  useEffect(() => {
+    async function getWeather(latitude: number, longitude: number) {
+      try {
         const response = await axios.get(
-          'https://api.gismeteo.net/v2/weather/current/4368/?lang=en',
-          {
-            headers: {
-              'X-Gismeteo-Token': '56b30cb255.3443075',
-              'Accept-Encoding': 'gzip, deflate, br' 
-            }
-          }
+          `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,wind_speed_10m,weather_code`
         );
-        console.log(response)
-        }
-        catch(error){
-            setTemperature(-999)
-            setWeather('ОШИБКА')
-            console.log('ошибка')
-        }
-        
-}
-getWeather()
-    }, [weather, temperature])
-    return(
-        <div>
-            <h1>{weather}</h1>
-            <h1>{temperature}</h1>
-        </div>
-    )
-}
+        console.log(response.data.current.temperature_2m);
+        setTemperature(`${response.data.current.temperature_2m}°C`);
+        setWeather(response.data.current.weather_code);
+      } catch (error) {
+        setTemperature("-99");
+        setWeather(900);
+        console.log("ошибка");
+      }
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLatitude(Math.round(position.coords.latitude * 100) / 100);
+      setLongtitude(Math.round(position.coords.longitude * 100) / 100);
+      console.log(`Получены координаты ${latitude} ${longitude}`);
+    });
+
+    getWeather(latitude, longitude);
+  }, [weather, temperature, latitude, longitude]);
+  return (
+    <div>
+      <h1>{currentWeatherMessage}</h1>
+      <h1>{temperature}</h1>
+    </div>
+  );
+};
 
 export default WeatherWidget;
