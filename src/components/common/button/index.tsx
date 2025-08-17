f5cbc8de3d714a80ac9af4ce348427f9bc345103
DanilaBesk <danilbeskorovaev546@gmail.com>
2025-08-17 17:29:02 +0300

    style: improve button component

--- src/components/common/button/index.tsx
+++ src/components/common/button/index.tsx
@@ -1,27 +1,22 @@
 import React from 'react';
-import styles from './button.module.css';
+import styles from './index.module.css';
 import cn from 'classnames';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: 'default' | 'accent' | 'danger' | 'ghost';
+  variant?: 'default' | 'accent' | 'danger' | 'icon';
   compSize?: 'sm' | 'md' | 'lg';
 }
 
 export default function Button({
+  children,
   variant = 'default',
   compSize = 'md',
   className,
   ...props
 }: ButtonProps) {
   return (
-    <button
-      className={cn(
-        styles.button,
-        styles[variant],
-        styles[compSize],
-        className,
-      )}
-      {...props}
-    />
+    <button className={cn(styles.button, styles[variant], styles[compSize], className)} {...props}>
+      {children}
+    </button>
   );
 }
